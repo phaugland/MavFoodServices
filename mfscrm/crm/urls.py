057c264e7c9e5909dd4481093ba5fc7ff8953e3b
phaugland <47875853+phaugland@users.noreply.github.com>
2020-02-29 04:05:43 -0600

    Add edit customer functionality

--- mfscrm/crm/urls.py
+++ mfscrm/crm/urls.py
@@ -6,5 +6,5 @@ app_name = 'crm'
 urlpatterns = [
 
     path('customer_list', views.customer_list, name='customer_list'),
-
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
 ]
--- mfscrm/crm/views.py
+++ mfscrm/crm/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from .forms import *
 
@@ -7,3 +7,22 @@ def customer_list(request):
     customer = Customer.objects.filter(created_date__lte=timezone.now())
     return render(request, 'crm/customer_list.html',
                  {'customers': customer})
+
+def customer_edit(request, pk):
+   customer = get_object_or_404(Customer, pk=pk)
+   if request.method == "POST":
+       # update
+       form = CustomerForm(request.POST, instance=customer)
+       if form.is_valid():
+           customer = form.save(commit=False)
+           customer.updated_date = timezone.now()
+           customer.save()
+           customer = Customer.objects.filter(created_date__lte=timezone.now())
+           return render(request, 'crm/customer_list.html',
+                         {'customers': customer})
+   else:
+        # edit
+       form = CustomerForm(instance=customer)
+
+   return render(request, 'crm/customer_edit.html', {'form': form})
+
